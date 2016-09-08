810076b049994b64661c3bdb59db00faf0bb8984
Yen-Chin Lee <coldnew.tw@gmail.com>
2016-09-08 18:10:35 +0800

    first commit
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

---
+++ slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+   // Slide Settings
+   settings: {
+     title: 'Clojure Taiwan 讀書會 #8', 
+     subtitle: 'Clojure for the BRAVE and True: ch10', 
+     useBuilds: true, // Default: true. False will turn off slide animation builds. 
+     usePrettify: false, // Default: true 
+     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+     favIcon: 'images/clojuretw-icon.png', 
+     fonts: [
+       'Open Sans:regular,semibold,italic,italicsemibold',
+       'Source Code Pro'
+     ],
+   }, 
+ 
+   // Author information
+   presenters: [{
+     name: 'coldnew', 
+     company: '', 
+     gplus: '', 
+     twitter: '', 
+     www: 'https://coldnew.github.io', 
+     github: 'http://github.com/coldnew', 
+   }]
+};
